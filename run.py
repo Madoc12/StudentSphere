cba55d5c08e1d82696cc7512610c1af9c520363e
Madoc12 <168454044+Madoc12@users.noreply.github.com>
2025-12-10 07:55:56 +0000

    Clean up imports in run.py
    Removed unused imports for Teacher and Student models.

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 from student_data import app, db
 import student_data.routes
-# from student_data.models import Teacher, Student
-# from flask_login import current_user
 
 
 if __name__ == "__main__":
